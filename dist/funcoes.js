ce57ec33c66123da094000872f8a888384c0918c
Alex <alex_soares_oliveira@hotmail.com>
2024-07-25 20:35:18 -0300

    utilizando novos metodos

---
+++ dist/funcoes.js
@@ -0,0 +1,18 @@
+"use strict";
+function calculaArea(base, altura) {
+    Number;
+    return base * altura;
+}
+const calculaArea2 = (base, altura) => base * altura;
+function somar(...numeros) {
+    console.log(numeros);
+}
+function teste() {
+    if (10 > 5) {
+        return 'dez maior que cinco';
+    }
+    else {
+        return 5;
+    }
+}
+const resultadoDeTeste = teste();
---
+++ dist/tipos_basicos.js
@@ -0,0 +1,18 @@
+"use strict";
+let estaChovendo = false;
+estaChovendo = true;
+let idade = 27;
+let altura = 1.75;
+const nacionalidade = 'brasileira';
+//array
+const colegas = ['Lucas', 'Fernanda'];
+const tecnologias = ['html', 'css', 'js'];
+const notas = [7, 9, 5, 8];
+const lista = ['Alex', true, 27];
+let idadeDaAna = 25;
+idadeDaAna = '25 Anos';
+let dadosDaApi;
+dadosDaApi = 10;
+dadosDaApi = true;
+dadosDaApi = [1, 2, 3];
+dadosDaApi = 'string';
